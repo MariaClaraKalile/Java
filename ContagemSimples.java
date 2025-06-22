804d2ce97267bcec74bb8c2c97f1bdaaf9f4dcd3
Maria Clara Kalile <105286222+MariaClaraKalile@users.noreply.github.com>
2025-06-22 16:36:17 -0300

    Add files via upload
    Programa que realiza a listagem de 1 a 10 através do laço (for).

---
+++ ContagemSimples.java
@@ -0,0 +1,7 @@
+public class ContagemSimples {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) { // ++ significa um operador de incremento, ele adiciona 1 ao valor atual da variável | for é um loop
+            System.out.println(i);
+        }
+    }
+}
