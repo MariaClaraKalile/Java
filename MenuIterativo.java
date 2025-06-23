5b14a8b444cb8e5d50cefb2e15969fb6127cc8f7
Maria Clara Kalile <105286222+MariaClaraKalile@users.noreply.github.com>
2025-06-22 22:46:08 -0300

    Add files via upload

---
+++ MenuIterativo.java
@@ -0,0 +1,43 @@
+
+// 3)_Menu Interativo (do-while)
+//Desenvolva um programa que exiba um menu com
+// opções (1-Somar, 2-Subtrair, 3-Sair) e continue executando
+// até que o usuário escolha a opção 3, utilizando do-while.
+
+import java.util.Scanner;
+
+public class MenuIterativo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int opcao = 0;
+
+        do {
+            System.out.println("------------------------");
+            System.out.println("---  MENU ITERATIVO  ---");
+            System.out.println("------------------------");
+            System.out.println("Digite o primeiro numero: ");
+            double n1 = input.nextDouble();
+            System.out.println("Digite o segundo numero: ");
+            double n2 = input.nextDouble();
+            System.out.println("Selecione a opção desejada:");
+            System.out.println("[ 1 ] - Somar");
+            System.out.println("[ 2 ] - Subtrair");
+            System.out.println("[ 3 ] - Sair");
+            opcao = input.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("A soma dos dois números é: " + (n1 + n2));
+                    break;
+                case 2:
+                    System.out.println("A subtração dos dois números é: " + (n1 - n2));
+                    break;
+                    case 3:
+                        System.out.println("Saindo...");
+                        break;
+            }
+        } while (opcao != 3);
+
+        input.close();
+
+    }
+}
